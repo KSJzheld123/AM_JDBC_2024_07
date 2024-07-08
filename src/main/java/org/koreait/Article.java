0c69ac6e017cd8863f625fc0fa49c3519233e396
ksjzheld123 <rlatjswnzheld970616@gmail.com>
2024-07-08 17:15:47 +0900

    d240708

--- src/main/java/org/koreait/Article.java
+++ src/main/java/org/koreait/Article.java
@@ -1,83 +1,104 @@
 package org.koreait;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.*;
+import java.time.LocalDate;
 import java.util.Scanner;
+
 
 public class Article {
 
-    List<ArticleList> articles = new ArrayList<>();
-    Scanner sc = new Scanner(System.in);
-    int lastId = 0;
-
-    public void run() {
-
-        System.out.printf("명령어 ) ");
-        String cmd = sc.nextLine().trim();
-
-        while (true) {
-
-            if(cmd.equals("exit")) {
-                break;
+    // 연결
+    public class DBConnection {
+        public static Connection getConnection() {
+            Connection conn = null;
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                String url = "jdbc:mariadb://localhost:3306/AM_JDBC_2024_07";
+                conn = DriverManager.getConnection(url, "root", "");
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-
-            if(cmd.length() ==  0) {
-                System.out.println("명령어를 입력해 주세요");
-                run();
-            }
-
-            if (cmd.equals("article write")) {
-                write();
-            } else if (cmd.equals("article list")) {
-                list();
-            } else {
-                System.out.println("명령어를 다시 입력해주세요");
-                run();
-            }
-
-            System.out.println("시스템 종료");
+            return conn;
         }
     }
 
+    static Connection conn = null;
+    static PreparedStatement stmt = null;
+    static ResultSet rs = null;
 
+    public static void write(Scanner sc) {
+        System.out.println("제목을 입력해주세요 ");
+        String titleAdd = sc.next();
+        System.out.println("내용을 입력해주세요");
+        String bodyAdd = sc.next();
+        LocalDate now = LocalDate.now();
+        String now2 = String.valueOf(now);
+        try {
 
+            String sql = "INSERT INTO " + "`article` " + "SET `regDate` = " + "?, " + "`updateDate` = " + "?, " + "`title` = " + "?, " + "`body` = " + "? ";
 
+            stmt = conn.prepareStatement(sql);
 
+            stmt.setString(1, now2);
+            stmt.setString(2, now2);
+            stmt.setString(3, titleAdd);
+            stmt.setString(4, bodyAdd);
 
+            stmt.executeUpdate();
 
+            rs.close();
+            stmt.close();
+            conn.close();
 
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
 
-
-
-
-
-
-
-    void write() {
-        int id = lastId + 1;
-        System.out.printf("제목 : ");
-        String title = sc.nextLine();
-        System.out.printf("내용 : ");
-        String body = sc.nextLine();
-
-        ArticleList article = new ArticleList(id, title, body);
-        articles.add(article);
-        lastId++;
-
-        System.out.println(id + "번 게시글이 생성되었습니다");
-        run();
-    }
-
-    void list() {
-        System.out.println(" 번호  ||  제목  ||   내용   ");
-        for (int i = articles.size() - 1; i >= 0; i--) {
-            String titleCut = articles.get(i).title;
-            if(articles.get(i).title.length() > 3) {
-                titleCut = articles.get(i).title.substring(0,3);
-            }
-            System.out.printf("   %d   ||   %s  ||   %s   \n", articles.get(i).id, titleCut, articles.get(i).body);
+            System.out.println("게시글이 생성되었습니다");
         }
-        run();
     }
 
+//    public static void list(Scanner sc) {
+//        String viewStr1 = "SELECT * FROM article";
+//        ResultSet result1 = stmt.executeQuery(viewStr1);
+//
+//    }
+
+
+    // 실행
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        conn = DBConnection.getConnection();
+        boolean loop = true;
+
+        while (loop) {
+            System.out.println("1.게시글 작성 2.게시글 리스트");
+            int input = sc.nextInt();
+            switch (input) {
+                case 1:
+                    write(sc);
+                    break;
+                case 2:
+                    break;
+
+                case 3:
+                    loop = false;
+                    sc.close();
+                    try {
+                        stmt.close();
+                        rs.close();
+                        conn.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+
+
+
+
+            }
+        }
+    }
 }
